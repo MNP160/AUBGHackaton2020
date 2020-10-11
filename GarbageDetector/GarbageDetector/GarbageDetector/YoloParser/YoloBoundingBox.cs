4a09fa41eff93ce45c9a2d0583670a6fb97105e2
Mladen Penev <mpenev13@gmail.com>
2020-10-11 13:31:11 +0300

    adding model

---
+++ GarbageDetector/GarbageDetector/GarbageDetector/YoloParser/YoloBoundingBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace GarbageDetector.YoloParser
+{
+    public class BoundingBoxDimensions : DimensionsBase { }
+
+    public class YoloBoundingBox
+    {
+        public BoundingBoxDimensions Dimensions { get; set; }
+        public string Label { get; set; }
+        public float Confidence { get; set; }
+        public Color BoxColor { get; set; }
+        public RectangleF Rectangle
+        {
+            get { return new RectangleF(Dimensions.X, Dimensions.Y, Dimensions.Width, Dimensions.Height); }
+        }
+
+    }
+
+}
